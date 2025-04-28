1da11771fb53c28057b52f1af82bbdc92b4521ed
Dylan Oldham <dylansoldham@gmail.com>
2025-04-28 11:34:13 -0700

    Header is now populated and ROM initialized

--- src/rom.rs
+++ src/rom.rs
@@ -11,34 +11,43 @@ pub enum MappingMode {
 }
 
 #[derive(Default)]
+pub struct RomHeader {
+    pub title: String,
+    pub mode: MappingMode,
+    pub chipset: u8,
+    pub rom_size: usize,
+    pub ram_size: usize,
+    pub country: u8,
+    pub dev_id: u8,
+    pub version: u8,
+    pub checksum: u16,
+}
+
+#[derive(Default)]
 pub struct RomFile {
-    title: String,
-    mode: MappingMode,
-    chipset: u8,
-    rom_size: usize,
-    ram_size: usize,
-    country: u8,
-    dev_id: u8,
-    rom_version: u8,
-    checksum: u16,
-    checksum_complement: u16,
-    rom: Vec<u8>,
-    ram: Vec<u8>,
+    pub header: RomHeader,
+    pub rom: Vec<u8>,
+    pub ram: Vec<u8>,
 }
 
 impl RomFile {
     pub fn new(path: &Path) -> Result<Self, EmulatorError> {
 
-        let mut data = Self::read_data(path)?;
-        Self::remove_file_header(&mut data);
-        let rom_header = Self::find_rom_header(&data)?;
-        
-        let title = String::from_utf8_lossy(&rom_header[0..21]).into_owned();
-        println!("Title: {}", title);
+        let mut rom = Self::read_data(path)?;
+        Self::remove_file_header(&mut rom);
+        let header = Self::find_rom_header(&rom)?;
+
+        if 0x400 * header.rom_size != rom.len() {
+            return Err(EmulatorError::RomError(format!("ROM size {} does not match header {}", rom.len(), 0x400 * header.rom_size)))
+        }
+
+        let mut ram = Vec::new();
+        ram.resize(0x400 * header.ram_size, 0u8);
 
         Ok(RomFile {
-            title,
-            ..Default::default()
+            header,
+            rom,
+            ram,
         })
     }
 
@@ -57,14 +66,13 @@ impl RomFile {
         }
     }
 
-    fn find_rom_header(data: &Vec<u8>) -> Result<&[u8], EmulatorError> {
-
+    fn find_rom_header(data: &Vec<u8>) -> Result<RomHeader, EmulatorError> {
         let checksum = data
             .iter()
             .map(|v| *v as u16)
             .fold(0u16, |sum, byte| sum.overflowing_add(byte).0);
 
-        [0x007FC0, 0x00FFC0, 0x40FFC0]
+        let header_data = [0x007FC0, 0x00FFC0, 0x40FFC0]
             .into_iter()
             .find_map(|base: usize| -> Option<&[u8]> {
                 if data.len() < base+512 {
@@ -77,6 +85,34 @@ impl RomFile {
                 }
                 None
             })
-            .ok_or(EmulatorError::RomError("Failed to find ROM header".to_owned()))
+            .ok_or(EmulatorError::RomError("Failed to find ROM header".to_owned()))?;
+
+        let title = String::from_utf8_lossy(&header_data[0..21]).into_owned();
+        let mode = match header_data[21] & 0b111 {
+            0 => MappingMode::LoRom,
+            1 => MappingMode::HiRom,
+            5 => MappingMode::ExHiRom,
+            byte => return Err(EmulatorError::RomError(format!("Invalid mapping mode {}", byte).to_owned()))
+        };
+        let chipset = header_data[22];
+        let rom_size = 1 << header_data[23];
+        let ram_size = 1 << header_data[24];
+        let country = header_data[25];
+        let dev_id = header_data[26];
+        let version = header_data[27];
+
+        Ok(RomHeader {
+            title,
+            mode,
+            chipset,
+            rom_size,
+            ram_size,
+            country,
+            dev_id,
+            version,
+            checksum,
+        })
     }
+
+
 }
